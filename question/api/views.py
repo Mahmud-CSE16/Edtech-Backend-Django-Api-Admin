252a34af91e9a5b3cc2f70a006de82df22f59aa4
Mahmud_CSE16 <mahamudul7959@gmail.com>
2020-07-09 11:25:58 +0600

    API KYE Fixed

--- question/api/views.py
+++ question/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.generics import ListAPIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from mathbyrony.api_permissons import Check_API_KEY_Auth
 
 from question.models import Question
 from question.api.serializers import QuestionSerializer
@@ -31,7 +32,7 @@ class QuestionListAPIView(ListAPIView):
     queryset = Question.objects.all().filter(published=True).order_by("?")
     serializer_class = QuestionSerializer
     authentication_classes = [TokenAuthentication,]
-    permission_classes = [IsAuthenticated,]
+    permission_classes = [IsAuthenticated & Check_API_KEY_Auth,]
     pagination_class = PageNumberPagination
     filter_backends = [SearchFilter, OrderingFilter,DjangoFilterBackend,]
     filterset_fields = ['subcategories','chapters','language','histories','types',]

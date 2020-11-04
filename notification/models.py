d1eb62d2c092bc77bcb25c73a41654d7fc378708
Mahmud_CSE16 <mahamudul7959@gmail.com>
2020-11-05 00:45:04 +0600

    notification issue fixed

--- notification/models.py
+++ notification/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from pyfcm import FCMNotification
 from django.conf import settings
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.db.models.signals import m2m_changed
 
 
 # Create your models here.
@@ -23,15 +24,19 @@ class Notification(models.Model):
 
     def save(self, *args, **kwargs):
 
+        print(self.published)
+        #save
+        super(Notification, self).save(*args, **kwargs)
+
         if(self.published == True):
             self.published_time = datetime.now()
-
+            
             push_service = FCMNotification(api_key=settings.FCM_SERVER_KEY)
 
             # Conditional topic messaging
             #topic_condition = ""
 
-            # print(self.categories.all())
+            print(self.subCategories.all())
 
             for category in self.subCategories.all():
                 #topic_condition += "'{}' in topics || ".format(category.name)
@@ -102,4 +107,41 @@ class Notification(models.Model):
     def __str__(self):
         return self.title
 
-    
+
+
+""" after SubCategory added """
+def subCategories_changed(sender, instance, action, reverse, *args, **kwargs):
+
+    print("inside subCategories_changed")
+    if(instance.published == True):
+            
+        push_service = FCMNotification(api_key=settings.FCM_SERVER_KEY)
+
+        # Conditional topic messaging
+        #topic_condition = ""
+
+        print(instance.subCategories.all())
+
+        for category in instance.subCategories.all():
+            #topic_condition += "'{}' in topics || ".format(category.name)
+
+            data_message = {
+                "click_action": 'FLUTTER_NOTIFICATION_CLICK',
+                "title": instance.title,
+                "short_description": instance.short_description,
+                "long_description": instance.long_description,
+                # "image_url": event.image_url
+            }
+
+            result = push_service.notify_topic_subscribers(
+                topic_name="all", #category.name
+                #condition = topic_condition,
+                message_title=instance.title, 
+                message_body=instance.short_description,
+                click_action = 'FLUTTER_NOTIFICATION_CLICK',
+                data_message = data_message
+            )
+           
+    pass
+
+m2m_changed.connect(subCategories_changed, sender=Notification.subCategories.through)
